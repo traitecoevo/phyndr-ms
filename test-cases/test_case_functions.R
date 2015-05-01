f320218a2e4aca6cd43f1df0b0a93ac0f061a977
matthew pennell <mwpennell@gmail.com>
2015-05-01 17:30:02 -0400

    even moar cites

--- test-cases/test_case_functions.R
+++ test-cases/test_case_functions.R
@@ -15,3 +15,19 @@ sr<-calculate.overlap("Vascular_Plants_rooted.dated.tre",wood$gs)
 wood<-read.csv("GlobalWoodinessDatabase.csv")
 sr<-calculate.overlap("Conifer-timetree.tre",wood$gs)
 
+##
+library(phyndr)
+library(TaxonLookup)
+
+t <- read.tree("Vascular_Plants_rooted.dated.tre")
+
+plant_lookup[-duplicated(plant_
+## build taxonomy from order names
+gen <- sapply(t$tip.label, function(x) strsplit(x, split="_")[[1]][1])
+## drop tips of genera that don't match
+to_drop <- names(gen[-which(gen %in% plant_lookup$genus)])
+phy <- drop.tip(t, to_drop)
+gen <- gen[-which(names(gen) %in% to_drop)]
+tax <- lapply(as.character(gen), function(x) subset(plant_lookup, genus == x))
+tax <- do.call(rbind, tax)
+rownames(tax) <- phy$tip.label
